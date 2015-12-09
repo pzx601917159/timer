b77f324c70d203474fc51e9d951c793e5ee978b8
pzx601917159 <601917159@qq.com>
2015-12-09 18:06:57 +0800

    修改了定时器类

--- wheeltimer.h
+++ wheeltimer.h
@@ -8,9 +8,35 @@
 #ifndef _WHEELTIMER_H
 #define _WHEELTIMER_H
 
+//时间轮定时器
 class WheelTimer
 {
+    public:
     WheelTimer();
+    ~WheelTimer();
+
+    WheelTimer* GetPrev();
+    WheelTimer* GetNext();
+
+    WheelTimer SetPrev(WheelTimer* prev);
+    WheelTimer SetNext(WheelTimer* next);
+
+    void OnTimer();
+
+    uint32_t GetRemain() const;
+    void SetRemain(uint32_t remain);
+    void OnSchedule(WheelNode* node);
+
+    bool Schedule(TimerTask* task,uint32_t minsec);
+    void Destroy();
+
+    private:
+    TimerTask* m_task;  //定时任务
+    WheelNode* m_node;  //轮子的槽
+    uint32_t m_remain;  //剩余的时间
+    WheelTimer* m_prev; //前一个定时器
+    WheelTimer* m_next; //后一个定时器
+
 };
 
 #endif //_WHEELTIMER_H
