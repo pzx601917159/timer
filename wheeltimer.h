709dc994c90f10afa21439da94cfc4e80cc73a23
pzx601917159 <601917159@qq.com>
2015-12-11 13:10:06 +0800

    定时器初始版本，已经可以使用

--- wheeltimer.h
+++ wheeltimer.h
@@ -2,41 +2,43 @@
 	> File Name: wheeltimer.h
 	> Author: 彭治湘
 	> Mail: 345109788@qq.com 
-	> Created Time: Wednesday 09 December 2015 05:36:30 PM CST
+	> Created Time: Thursday 10 December 2015 12:23:24 PM CST
  ********************************************************************/
 
+//时间轮定时器
 #ifndef _WHEELTIMER_H
 #define _WHEELTIMER_H
+#include <inttypes.h>
+#include <list>
 
-//时间轮定时器
+class Timer;
+class Task;
+class Wheel;
+class WheelNode;
+
 class WheelTimer
 {
     public:
+    //构造
     WheelTimer();
+    //析构
     ~WheelTimer();
-
-    WheelTimer* GetPrev();
-    WheelTimer* GetNext();
-
-    WheelTimer SetPrev(WheelTimer* prev);
-    WheelTimer SetNext(WheelTimer* next);
-
-    void OnTimer();
-
-    uint32_t GetRemain() const;
-    void SetRemain(uint32_t remain);
-    void OnSchedule(WheelNode* node);
-
-    bool Schedule(TimerTask* task,uint32_t minsec);
-    void Destroy();
+    void InsertTimer(Timer* timer);
+    bool Click();
+    //回调函数的固定格式
+    static void OnAlarm(int);
+    Timer* CreateTimer(Task* task,uint32_t time);
 
     private:
-    TimerTask* m_task;  //定时任务
-    WheelNode* m_node;  //轮子的槽
-    uint32_t m_remain;  //剩余的时间
-    WheelTimer* m_prev; //前一个定时器
-    WheelTimer* m_next; //后一个定时器
+    void InitTimer();
+    //这里只能是静态函数，不能是成员函数
+    static uint32_t GetAlarmCount();
 
+    typedef std::list<Wheel*> WheelList;
+    WheelList m_wheelList;  //时间轮链表
+    uint32_t m_unitTime;    //单元时间
+    uint32_t m_count;       //时间轮的数量
+    WheelNode* m_node;      //当前的时间槽
+    static uint32_t m_alarmCount;//alarm信号出发了几次
 };
-
-#endif //_WHEELTIMER_H
+#endif //_WHEELTIMER_
